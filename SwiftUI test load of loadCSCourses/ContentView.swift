c980a9c0a6cf1839d15c509a2ff816f8af7c9952
tcowan <tcowan@my321.net>
2022-05-24 07:48:42 -0600

    Initial Commit

---
+++ SwiftUI test load of loadCSCourses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI test load of loadCSCourses
+//
+//  Created by Ted Cowan on 5/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI test load of loadCSCourses/SwiftUI_test_load_of_loadCSCoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_test_load_of_loadCSCoursesApp.swift
+//  SwiftUI test load of loadCSCourses
+//
+//  Created by Ted Cowan on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_test_load_of_loadCSCoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
